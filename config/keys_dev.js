cb914f97a52ecbbfa6f1edf3945895c50c9dea92
Bilal-khursheed <Bilal.khurseed617@gmail.com>
2022-02-02 17:47:21 +0500

    typing game exp

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://bilal-khursheed:bilal-khursheed@cluster0.vmkav.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrKey: 'bilal'
+  }
